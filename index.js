d4203cddd58b51f1fce38175d9594d01f31706f2
Geza Kovacs <gkovacs@stanford.edu>
2017-03-27 02:40:17 -0700

    initial commit

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-import $ from 'jquery'
 import Datamap from 'datamaps'
 import CSV from 'csv-string'
 import polylabel from 'polylabel'
@@ -8,13 +7,15 @@ window.Datamap = Datamap
 //$(document.body).append("<h1>Hello World again v2!</h1>");
 
 //var map = new Datamap({element: document.getElementById('container')});
-
+/*
 var country_list = Datamap.prototype.worldTopo.objects.world.geometries.map((x) => [x.id, x.properties.name])
-var country_id_list = country_list.map(x => x.id)
+var country_id_list = country_list.map(x => x[0])
 var country_name_to_id = {}
 for (let [country_id,country_name] of country_list) {
   country_name_to_id[country_name] = country_id
 }
+*/
+/*
 var country_aliases = {
   // supported country names are at http://datamaps.github.io/scripts/0.4.4/datamaps.world.js
   "UAE": "United Arab Emirates",
@@ -25,24 +26,60 @@ var country_aliases = {
   "UK": "United Kingdom",
   "Palestine": "Israel"
 }
+*/
+
+var country_aliases = {
+  // https://raw.githubusercontent.com/lukes/ISO-3166-Countries-with-Regional-Codes/master/all/all.json
+  "Vietnam": "Viet Nam",
+  "Palestine": "Israel",
+  "Iran": "Iran (Islamic Republic of)",
+  "Russia": "Russian Federation",
+  "South Korea": "Korea (Republic of)",
+  "Taiwan": "Taiwan, Province of China",
+  "UAE": "United Arab Emirates",
+  "UK": "United Kingdom of Great Britain and Northern Ireland",
+  "USA": "United States of America",
+  "Venezuela": "Venezuela (Bolivarian Republic of)",
+}
+
 /*
-var arc_id_to_arc_array = Datamap.prototype.worldTopo.arcs
-var country_id_to_polygon = {}
+let arc_id_to_arc_array = Datamap.prototype.worldTopo.arcs
+let country_id_to_polygon = {}
 for (let country_info of Datamap.prototype.worldTopo.objects.world.geometries) {
-  var polygon = {}
+  let polygon = {}
   polygon.coordinates = country_info.arcs.map(coordinate_id_array => coordinate_id_array.map(arc_id => arc_id_to_arc_array[arc_id]))
   country_id_to_polygon[country_info.id] = polygon
 }
-*/
-//var country_id_to_center = {}
-//for (let country_id of country_id_list) {
-  
-//}
+let country_id_to_center = {}
+if (localStorage.country_id_to_center) {
+  country_id_to_center = JSON.parse(localStorage.country_id_to_center)
+} else {
+  for (let country_id of country_id_list) {
+    var polygon = country_id_to_polygon[country_id]
+    country_id_to_center[country_id] = polylabel(polygon.coordinates, 1.0)
+  }
+  localStorage.country_id_to_center = JSON.stringify(country_id_to_center)
+}
 //console.log(country_id_to_polygon)
+*/
 
 async function main() {
   let data_request = await fetch('CRP_Participants - Countries Final.csv')
   let data_text = await data_request.text()
+  let country_codes_map_request = await fetch('country_codes.json')
+  let country_codes_map = await country_codes_map_request.json()
+  let country_name_to_id = {}
+  for (let country_info of country_codes_map) {
+    country_name_to_id[country_info.name] = country_info['alpha-2']
+  }
+  /*
+  let country_label_points_request = await fetch('ne_10m_admin_0_label_points-country.json')
+  let country_label_points_text = await country_label_points_request.text()
+  let country_id_to_center = {}
+  for (let country_info of JSON.parse(country_label_points_text).features) {
+    country_id_to_center[country_info.properties.sr_brk_a3] = country_info.geometry.coordinates
+  }
+  */
   let country_id_to_num_participants = {}
   for (let [country, count] of CSV.parse(data_text)) {
     count = parseInt(count)
@@ -58,67 +95,56 @@ async function main() {
     }
     country_id_to_num_participants[country_id] += count
   }
-  let country_id_and_num_participants = []
+  let data = []
   for (let country_id of Object.keys(country_id_to_num_participants)) {
     let count = country_id_to_num_participants[country_id]
-    country_id_and_num_participants.push([country_id, Math.log(count), count])
+    data.push({code: country_id, z: count})
   }
-  var series = country_id_and_num_participants;
+  console.log(data)
 
-  // from https://github.com/markmarkoh/datamaps/blob/master/src/examples/highmaps_world.html
-  //
-  // Datamaps expect data in format:
-  // { "USA": { "fillColor": "#42a844", numberOfWhatever: 75},
-  //   "FRA": { "fillColor": "#8dc386", numberOfWhatever: 43 } }
-  var dataset = {};
-  // We need to colorize every country based on "numberOfWhatever"
-  // colors should be uniq for every value.
-  // For this purpose we create palette(using min/max series-value)
-  var onlyValues = series.map(function(obj){ return obj[1]; });
-  var minValue = Math.min.apply(null, onlyValues),
-          maxValue = Math.max.apply(null, onlyValues);
-  // create color palette function
-  // color can be whatever you wish
-  var paletteScale = d3.scale.linear()
-          .domain([minValue,maxValue])
-          .range(["#EFEFFF","#02386F"]); // blue color
-  // fill dataset in appropriate format
-  series.forEach(function(item){ //
-      // item example value ["USA", 70]
-      var iso = item[0],
-          value = item[1],
-          realvalue = item[2];
-      dataset[iso] = { numberOfThings: value, realvalue: realvalue, fillColor: paletteScale(value) };
-  });
-  // render map
-  new Datamap({
-      element: document.getElementById('container'),
-      projection: 'mercator', // big world map
-      // countries don't listed in dataset will be painted with this color
-      fills: { defaultFill: '#F5F5F5' },
-      data: dataset,
-      geographyConfig: {
-          borderColor: '#DEDEDE',
-          highlightBorderWidth: 2,
-          // don't change color on mouse hover
-          highlightFillColor: function(geo) {
-              return geo['fillColor'] || '#F5F5F5';
-          },
-          // only change border
-          highlightBorderColor: '#B7B7B7',
-          // show desired information in tooltip
-          popupTemplate: function(geo, data) {
-              // don't show tooltip if country don't present in dataset
-              if (!data) { return ; }
-              // tooltip content
-              return ['<div class="hoverinfo">',
-                  '<strong>', geo.properties.name, '</strong>',
-                  '<br>Count: <strong>', data.realvalue, '</strong>',
-                  '<br>Log Count: <strong>', data.numberOfThings, '</strong>',
-                  '</div>'].join('');
-          }
-      }
-  });
+  Highcharts.mapChart('container', {
+        chart: {
+            borderWidth: 1,
+            map: 'custom/world'
+        },
+        title: {
+          text: ''
+        },
+        //title: {
+        //    text: 'Participation by Country'
+        //},
+
+        //subtitle: {
+        //    text: 'Demo of Highcharts map with bubbles'
+        //},
+
+        legend: {
+            enabled: false
+        },
+
+        mapNavigation: {
+            enabled: true,
+            buttonOptions: {
+                verticalAlign: 'bottom'
+            }
+        },
+
+        series: [{
+            name: 'Countries',
+            color: '#E0E0E0',
+            enableMouseTracking: false
+        }, {
+            type: 'mapbubble',
+            name: 'Population 2013',
+            joinBy: ['iso-a2', 'code'],
+            data: data,
+            minSize: 4,
+            maxSize: '12%',
+            tooltip: {
+                pointFormat: '{point.code}: {point.z}'
+            }
+        }]
+    });
 }
 
 main()
