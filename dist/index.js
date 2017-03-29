7e3c3ce8f41e1410ed170e0e20d3ecaa65987d02
Geza Kovacs <gkovacs@stanford.edu>
2017-03-28 22:20:24 -0700

    changed color to red

--- dist/index.js
+++ dist/index.js
@@ -18212,6 +18212,92 @@ async function main() {
   }
   console.log(data)
 
+Highcharts.theme = {
+   colors: ['#f45b5b', '#8085e9', '#8d4654', '#7798BF', '#aaeeee', '#ff0066', '#eeaaee',
+      '#55BF3B', '#DF5353', '#7798BF', '#aaeeee'],
+   chart: {
+      backgroundColor: null,
+      style: {
+         fontFamily: 'Signika, serif'
+      }
+   },
+   title: {
+      style: {
+         color: 'black',
+         fontSize: '16px',
+         fontWeight: 'bold'
+      }
+   },
+   subtitle: {
+      style: {
+         color: 'black'
+      }
+   },
+   tooltip: {
+      borderWidth: 0
+   },
+   legend: {
+      itemStyle: {
+         fontWeight: 'bold',
+         fontSize: '13px'
+      }
+   },
+   xAxis: {
+      labels: {
+         style: {
+            color: '#6e6e70'
+         }
+      }
+   },
+   yAxis: {
+      labels: {
+         style: {
+            color: '#6e6e70'
+         }
+      }
+   },
+   plotOptions: {
+      series: {
+         shadow: true
+      },
+      candlestick: {
+         lineColor: '#404048'
+      },
+      map: {
+         shadow: false
+      }
+   },
+
+   // Highstock specific
+   navigator: {
+      xAxis: {
+         gridLineColor: '#D0D0D8'
+      }
+   },
+   rangeSelector: {
+      buttonTheme: {
+         fill: 'white',
+         stroke: '#C0C0C8',
+         'stroke-width': 1,
+         states: {
+            select: {
+               fill: '#D0D0D8'
+            }
+         }
+      }
+   },
+   scrollbar: {
+      trackBorderColor: '#C0C0C8'
+   },
+
+   // General
+   background2: '#E0E0E8'
+
+};
+
+// Apply the theme
+Highcharts.setOptions(Highcharts.theme);
+
   Highcharts.mapChart('container', {
         chart: {
             borderWidth: 1,
@@ -18248,8 +18334,8 @@ async function main() {
             name: 'Population 2013',
             joinBy: ['iso-a2', 'code'],
             data: data,
-            minSize: 4,
-            maxSize: '12%',
+            minSize: 8,
+            maxSize: '13%',
             tooltip: {
                 pointFormat: '{point.code}: {point.z}'
             }
